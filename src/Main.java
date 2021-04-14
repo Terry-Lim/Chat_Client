4a3715aa3226507b5d2b407f139ef7fd44ea0b61
Terry-Lim <abcc200@naver.com>
2021-04-14 22:35:10 +0900

    프로젝트 생성

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+	private static final int PORT = 4900;
+	public static void main(String[] args) {
+		try {
+			Socket socket = new Socket("localhost", PORT);
+			DataInputStream dis = new DataInputStream(socket.getInputStream());
+			DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+			System.out.println("접속 완료");
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					new GUI_Login(dos, dis);					
+				}
+			});
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
+
