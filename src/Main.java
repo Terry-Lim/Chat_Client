f0a5f26e7bd46cbf5c599ec90463530dab2cf4e9
Terry-Lim <abcc200@naver.com>
2021-04-20 22:25:12 +0900

    방만들기, 방 찾기, 방입장 퇴장시수정

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
 import javax.swing.SwingUtilities;
-// 저장햇당
 
-//123
-
-/////아이고 좋아
 public class Main {
 
 	private static final int PORT = 4900;
@@ -18,11 +21,13 @@ public class Main {
 			Socket socket = new Socket("localhost", PORT);
 			DataInputStream dis = new DataInputStream(socket.getInputStream());
 			DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+			PrintWriter pw = new PrintWriter(new BufferedOutputStream(socket.getOutputStream()));
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			System.out.println("접속 완료");
 			SwingUtilities.invokeLater(new Runnable() {
 				@Override
 				public void run() {
-					new GUI_Login(dos, dis);					
+					new GUI_Login(dos, dis,  pw, br);					
 				}
 			});
 			
@@ -33,5 +38,4 @@ public class Main {
 		
 	}
 }
-// 안녕하세요 
-//  반갑습니다
+
